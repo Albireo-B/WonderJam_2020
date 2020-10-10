3cc4020841076d897aaf884b34e1505ead69531c
Guillaume <guilaume.vinet.38@gmail.com>
2020-10-10 16:00:21 +0200

    Add switching mode functionnality with outlines on dead bodies

--- Screw_Gravity/Game.gd
+++ Screw_Gravity/Game.gd
@@ -3,14 +3,18 @@ extends Spatial
 
 onready var tween = get_node("Movement_Tween")
 onready var cameraRaycast = get_node("Camera/RayCast")
+onready var letterScene = preload("res://letter.tscn")
+
 var coloredMaterial = SpatialMaterial.new()
 var xSpaceLetters = -3
 var activatedLetterList = Array()
 var selectedLetter = null
+var selectedbody = null
+var inLetterGame = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var letterScene = load("res://letter.tscn")
+	switchMode()
 	var alpha = "a".to_ascii()
 	for i in range(0,26):
 		var letter = letterScene.instance()
@@ -23,11 +27,46 @@ func _ready():
 
 
 func _process(delta):
-	if cameraRaycast.is_colliding():
-		selectedLetter = cameraRaycast.get_collider()
-		coloredMaterial.albedo_color = Color.red
-		selectedLetter.get_node("MeshInstance").set_material_override(coloredMaterial)
-		clearAllLettersExceptSelectedOneOhGodWhatHaveIDone(selectedLetter)
+	if inLetterGame:
+		if cameraRaycast.is_colliding():
+			selectedLetter = cameraRaycast.get_collider()
+			coloredMaterial.albedo_color = Color.red
+			selectedLetter.get_node("MeshInstance").set_material_override(coloredMaterial)
+			clearAllLettersExceptSelectedOneOhGodWhatHaveIDone(selectedLetter)
+		else :
+			clearAllLettersExceptSelectedOneOhGodWhatHaveIDone(null)
+	else:
+		if cameraRaycast.is_colliding():
+			selectedbody = cameraRaycast.get_collider()
+			selectedbody.get_node("Dead_body_one/Outline").visible = true
+		else :
+			clearBodiesOutline()
+
+func clearBodiesOutline():
+	if selectedbody:
+		selectedbody.get_node("Dead_body_one/Outline").visible = false
+	
+#deactivate collision mesh to allow the raycast to find the letter and inversely
+func switchMode():
+	clearBodiesOutline()
+	clearAllLettersExceptSelectedOneOhGodWhatHaveIDone(null)
+	if inLetterGame:
+		switchCollisions("Bodies",false)
+		switchCollisions("Letters",true)
+	else:
+		switchCollisions("Bodies",true)
+		switchCollisions("Letters",false)	
+
+func switchCollisions(objectsType,enabled):
+	if objectsType == "Bodies":
+		for N in get_node("Environment/Dead_Bodies").get_children():
+			for i in range (1,N.get_children().size()-1):
+				N.get_child(i).disabled = !enabled
+				print("Collision disabled for "+N.get_name())
+	else :
+		for N in get_node("Zone_Lettres/Plan").get_children():
+			N.get_node("CollisionShape").disabled = !enabled
+
 
 func clearAllLettersExceptSelectedOneOhGodWhatHaveIDone(letter):
 	for L in get_node("Zone_Lettres/Plan").get_children():
@@ -37,8 +76,12 @@ func clearAllLettersExceptSelectedOneOhGodWhatHaveIDone(letter):
 			L.get_node("MeshInstance").set_material_override(coloredMaterial)
 	
 func _input(event):
+	if event is InputEvent:
+		if event.is_action_pressed("ui_accept"):
+			inLetterGame = !inLetterGame
+			switchMode()
 	if event is InputEventMouseButton:
-		if event.pressed:
+		if event.pressed and inLetterGame:
 			if cameraRaycast.is_colliding() && !cameraRaycast.get_collider() in activatedLetterList :
 				activatedLetterList.append(cameraRaycast.get_collider())
 				moveLetter(cameraRaycast.get_collider())
